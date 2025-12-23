05e1f71420aa370d1bef54885e9dcd5c9cec2068
Patricia Grof <grof.patricia@veriqloud.fr>
2025-12-23 13:21:26 +0100

    Remove unnecessary secrets

--- generate_secrets.sh
+++ generate_secrets.sh
@@ -3,22 +3,6 @@
 # Define the target directory and secret files
 TARGET_DIR="roles/add_secrets/templates"
 mkdir -p "$TARGET_DIR"
-SECRETS=(
-  "jwt.secret.j2"
-  "session.secret.j2"
-  "storage_encryption_key.secret.j2"
-)
-
-# Generate each secret file
-for secret in "${SECRETS[@]}"; do
-  echo "Generating $secret..."
-  openssl rand -hex 64 > "$TARGET_DIR/$secret" || {
-    echo "Failed to generate $secret"
-    exit 1
-  }
-done
-
-echo "All secret files generated successfully in $TARGET_DIR/"
 
 # Genrate preshared key
 echo "Generating preshared key..."
